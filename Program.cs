665cee81dd548098f54514763f357e85dcb33e65
WhatIsFun <mohammed110051@gmail.com>
2023-06-14 14:29:15 +0400

    Fix Q1 and Q2

--- Program.cs
+++ Program.cs
@@ -12,48 +12,51 @@ namespace cSharp_Evaluation01
             //For example A = [4, 7, 3, 5, 1, 10, 90, 8] output of the array should
             //be[1, 3, 4, 5, 7, 8, 10, 90]
             int[] A = { 4, 7, 3, 5, 1, 10, 90, 8, 6 };
-            Console.WriteLine("Sorting array is: ");
-            //Console.WriteLine(arraySorting(A[i]));
-            int elementPos = 0;
+            int[] num = arraySorting(A);
+            Console.WriteLine("Sorted array: ");
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write(num[i]+" , ");
+            }
+            Console.WriteLine();
+            //int elementPos = 0;
             Console.WriteLine("Enter a number to start search in the array> ");
             int searchNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The serching number is "+ searchNum + " in "+ searchingNumber(elementPos,searchNum));
+            Console.WriteLine("The serching number is "+ searchNum + " in "+ searchingNumber(A,searchNum));
         }
 
         //Q1: Sorting method
-        //static int arraySorting(int[] A,int i)
-        //{
-        //    //loop from the second element to the last
-        //    for (i = 0; i < A.Length - 1; i++)
-        //    {
-        //        // Loop the array
-        //        for (int j = i+ 1; j < A.Length; j++)
-        //        {
-        //            if (A[i] > A[j]) // Comparing 
-        //            {
-        //                int num = A[i]; // Swaping elements
-        //                A[i] = A[j];
-        //                A[j] = num;
-        //            }
-        //        }
+        static int[] arraySorting(int[] A)
+        {
+            //loop from the second element to the last
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                // Loop the array
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    if (A[i] > A[j]) // Comparing 
+                    {
+                        int temp = A[i]; // Swaping elements
+                        A[i] = A[j];
+                        A[j] = temp;
+                    }
+                }
 
-        //     return A[i];
-        //    }
-        //}
+            }
+
+            return A;
+
+        }
         //Q2:
         //Write a method to implement searching
         static int searchingNumber(int[] A, int searchNum)
         {
-            for (int elementPos = 0; elementPos < A.Length; elementPos++)
+            for (int elementPos = 0;  elementPos < A.Length; elementPos++)
             {
 
                 if (A[elementPos] == searchNum)
                 {
                     return elementPos;
-                }
-                else
-                {
-                    Console.WriteLine("-1");
                 }
             }
             return -1;
